20cd1c71ef87bcf51cc45678cf8fe4026346ba99
Phodal Huang <h@phodal.com>
2017-11-17 15:29:52 +0800

    [T] setup jquery & bs

--- handler.js
+++ handler.js
@@ -10,14 +10,14 @@ const base_page = `<html lang="zh-cmn-Hans">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="keywords" content="Phodal, URL Shorter, shorter">
     <meta name="description" content="Landing Page">
-    <link href="https://cdn.bootcss.com/pure/1.0.0/pure-nr-min.css" rel="stylesheet">
+    <link href="https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" rel="stylesheet">
   </head>
   <body class="pure-g">
     <div class="pure-u-1-5">
     </div>
     <div class="pure-u-3-5">
       <div style="width: 300px; margin:100px auto;">
-        <h2><a href="https://www.phodal.com/">欢迎来到 Growth Studio</h1>
+        <h2>欢迎来到 Growth Studio</h1>
         <form method="POST" action="/todos" class="pure-form">
           <input type="text" placeholder="Email id="link" name="link" size="40" autofocus />
           <br/>
@@ -26,6 +26,8 @@ const base_page = `<html lang="zh-cmn-Hans">
         </form>
       </div>
     </div>
+    <script src="https://cdn.bootcss.com/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
   </body>
 </html>`
 
